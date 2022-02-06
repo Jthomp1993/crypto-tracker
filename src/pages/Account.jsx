3a23f8b4eee20fb648060492af577676c4032072
Joshua George Thompson <joshua@Joshuas-MacBook-Pro.local>
2022-02-06 21:07:48 +0000

    Register user

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,6 +1,8 @@
 import { useState, Fragment } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import Container from '@mui/material/Container';
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { doc, setDoc, serverTimestamp } from 'firebase/firestore';
+import { db } from '../firebase.config';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Item from '@mui/material/Grid';
@@ -9,16 +11,18 @@ import { makeStyles } from "@material-ui/core/styles";
 import Button from '@mui/material/Button';
 
 function Account() {
-    const [error, setError] = useState(false);
+    const [nameInvalid, setNameInvalid] = useState(false);
     const [emailInvalid, setEmailInvalid] = useState(false);
     const [passwordInvalid, setPasswordInvalid] = useState(false);
+    const [nameErrorText, setNameErrorText] = useState(null);
     const [emailErrorText, setEmailErrorText] = useState(null);
     const [passwordErrorText, setPasswordErrorText] = useState(null);
     const [formData, setFormData] = useState({
+        name: '',
         email: '',
         password: '',    });
 
-    const { email, password } = formData;
+    const { name, email, password } = formData;
 
     const useStyles = makeStyles({
         root: {
@@ -47,13 +51,21 @@ function Account() {
     const onChange = (e) => {
         setFormData((prevState) => ({
             ...prevState,
-            [e.target.type]: e.target.value
+            [e.target.id]: e.target.value
         }))
-
     }
 
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault();
+        var nameRe = /^[a-zA-Z]{3,}$/;
+        var testName = nameRe.test(formData.name);
+        if(testName === false) {
+            setNameInvalid(true);
+            setNameErrorText('Please enter your name.');
+        } else {
+            setNameInvalid(false);
+            setNameErrorText(null);
+        }
         var emailRe = /\S+@\S+\.\S+/;
         var testEmail = emailRe.test(formData.email);
         if(testEmail === false) {
@@ -68,17 +80,38 @@ function Account() {
         var testPassword = passwordRe.test(formData.password);
         if(testPassword === false) {
             setPasswordInvalid(true);
-            setPasswordErrorText('Please enter a password that contains between 6 to 20 characters and at least one uppercase and one lowercase letters.');
+            setPasswordErrorText('Please enter a password that contains between 6 and 20 characters and at least one uppercase and one lowercase letters.');
         } else {
             setPasswordInvalid(false);
             setPasswordErrorText(null);
         }
 
-        setTimeout(function() {
-            if(testEmail && testPassword) {
-                console.log('User registered');
+        if(testEmail && testPassword && testName) {
+            try {
+                const auth = getAuth();
+    
+                const userCredential = await createUserWithEmailAndPassword(
+                    auth,
+                    email,
+                    password
+                )
+    
+                const user = userCredential.user;
+    
+                updateProfile(auth.currentUser, {
+                    displayName: name
+                });
+    
+                const formDataCopy = {...formData}
+                delete formDataCopy.password
+                formDataCopy.timestamp = serverTimestamp();
+                await setDoc(doc(db, 'users', user.uid), formDataCopy);
+                
+                navigate('/');
+            } catch (error) {
+                console.log('Error');
             }
-        }, 0.500);
+        }
 
         
     }
@@ -90,10 +123,13 @@ function Account() {
                 <Grid container direction="column" justifyContent="center" alignItems="center">
                     <Grid item xs={12}>
                         <Item>
-                            <TextField error={emailInvalid} helperText={emailErrorText} onChange={onChange} required className={classes.root} sx={{  input: {color: 'white'}, mb: '1rem' }} id="outlined-basic" type="email" label="Email" variant="outlined"  />
+                            <TextField error={nameInvalid} helperText={nameErrorText} onChange={onChange} required className={classes.root} sx={{  input: {color: 'white'}, mb: '1rem' }} id="name" type="name" label="Name" variant="outlined"  />
                         </Item>
                         <Item>
-                            <TextField onChange={onChange} required error={passwordInvalid} helperText={passwordErrorText} className={classes.root} sx={{  input: {color: 'white'}, mb: '1rem' }} id="outlined-password-input" type="password" label="Password" variant="outlined" autoComplete="off" />
+                            <TextField error={emailInvalid} helperText={emailErrorText} onChange={onChange} required className={classes.root} sx={{  input: {color: 'white'}, mb: '1rem' }} id="email" type="email" label="Email" variant="outlined"  />
+                        </Item>
+                        <Item>
+                            <TextField onChange={onChange} required error={passwordInvalid} helperText={passwordErrorText} className={classes.root} sx={{  input: {color: 'white'}, mb: '1rem' }} id="password" type="password" label="Password" variant="outlined" autoComplete="off" />
                         </Item>
                         <Item>
                             <Button type='submit' sx={{ mt: '1rem', mx: '1rem', display: 'block', backgroundColor: '#13cf81' }} variant="contained">Sign In</Button>
